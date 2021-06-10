3300170c67a3e7e95b9918b88125701c232d4289
Hyowon Cho <63900561+Hyowon-Cho@users.noreply.github.com>
2021-06-10 09:37:21 -0500

    Making Calculator
    Making Calculator

---
+++ Learning/lecture3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+  double a = 9.8;
+  double b = 3.14;
+
+  double hap = a + b;
+  double cha = a - b;
+  double gop = a * b;
+  double mok = a / b;
+
+  printf("%f + %f = %f\n", a, b, hap);
+  printf("%f - %f = %f\n", a, b, cha);
+  printf("%f * %f = %f\n", a, b, gop);
+  printf("%f / %f = %f\n", a, b, mok);
+  
+}
+
+// Calculator
