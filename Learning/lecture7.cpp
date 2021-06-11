4b53b3054ed556a69ffdf3d859c528ae46cb6c7b
Hyowon Cho <63900561+Hyowon-Cho@users.noreply.github.com>
2021-06-11 01:11:35 -0500

    CASE, DEFAULT, GOTO, BREAK
    CASE, DEFAULT, GOTO, BREAK

---
+++ Learning/lecture7.cpp
@@ -0,0 +1,35 @@
+
+#include <stdio.h>
+
+int main() {
+	
+	int choice;
+
+	makeChoice:
+
+	printf("New game : 1\n");
+	printf("Save : 2\n");
+	printf("Option : 3\n");
+	printf("Credit : 4\n");
+
+	scanf_s("%d", &choice);
+	
+	switch (choice) {
+	case 1:
+		printf("This is New game!\n");
+		break;
+	case 2:
+		printf("This is Save!\n");
+		break;
+	case 3:
+		printf("This is Option!\n");
+		break;
+	case 4:
+		printf("This is Credit!\n");
+		break;
+	default:
+		printf("Please re-enter your message.\n");
+		goto makeChoice;
+		break;
+	}
+}
